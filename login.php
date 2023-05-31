140903161ec4fa3e7edad5045bdbe5b5d0a850f4
dilipsuthar60 <dilipsuthar212000@gmail.com>
2023-05-31 12:21:04 +0530

    login form

---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="contanier">
+        <div class="login-container">
+            <h1>Student</h1>
+            <h2>Welcome to Student daitails!</h2>
+            <p class="login-p">Please sign-in to your account and start the adventure</p>
+            <div>
+
+                <input placeholder="enter a name" type="text" id="name" name="name" />
+            </div>
+            <div>
+                <input placeholder="enter a password" type="password" id="password" name="password" />
+            </div>
+            <button class="login-btn">LOGIN</button>
+        </div>
+    </div>
+</body>
+
+</html>
