8202f8f2757304bd2ae5751f14259433220799ac
dilipsuthar60 <dilipsuthar212000@gmail.com>
2023-05-31 13:08:39 +0530

    login style

--- login.php
+++ login.php
@@ -1,3 +1,18 @@
+<?php
+include "config.php";
+$notmatch = NULL;
+if (isset($_POST["login"])) {
+    $username = $_POST["name"];
+    $password = $_POST["password"];
+    $sql = "SELECT * FROM `StudentData` WHERE `name` = '" . $username . "' AND  `password` = '" . $password . "'";
+    $result = $connect->query($sql);
+    if (mysqli_num_rows($result) > 0) {
+        header('location:display.php');
+    } else {
+        $notmatch = 1;
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,8 +25,12 @@
 </head>
 
 <body>
-    <div class="contanier">
+    <form class="contanier" method="POST">
         <div class="login-container">
+            <?php
+            if ($notmatch)
+                echo '<div class="error">Invalid Username and Password</div>';
+            ?>
             <h1>Student</h1>
             <h2>Welcome to Student daitails!</h2>
             <p class="login-p">Please sign-in to your account and start the adventure</p>
@@ -22,9 +41,9 @@
             <div>
                 <input placeholder="enter a password" type="password" id="password" name="password" />
             </div>
-            <button class="login-btn">LOGIN</button>
+            <button class="login-btn" type="login" name="login">LOGIN</button>
         </div>
-    </div>
+    </form>
 </body>
 
 </html>
