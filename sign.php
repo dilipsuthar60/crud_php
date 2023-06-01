dab8af4bf31a75565b5678ac2195b317726d7ba0
dilipsuthar60 <dilipsuthar212000@gmail.com>
2023-06-01 15:47:02 +0530

    sign page validation

--- sign.php
+++ sign.php
@@ -1,14 +1,45 @@
 <?php
 include "config.php";
-
+$signerror = NULL;
+$nameerror = "";
+$emailerror = "";
+$passworderror = "";
 if (isset($_POST["sign"])) {
     $signname = $_POST["name"];
     $signpassword = $_POST["password"];
     $signemail = $_POST["email"];
-    $sql = "INSERT INTO account VALUES ('$signname', '$signemail', '$signpassword')";
-    $result = $connect->query($sql);
-    if ($result) {
-        header('location:login.php');
+    if (empty($signname)) {
+        $nameerror = "name is required";
+        $signerror = 1;
+    } else if (!preg_match("/^[a-zA-Z ]*$/", $signname)) {
+        $nameerror = "name should be letter";
+        $signerror = 1;
+    }
+    if (empty($signemail)) {
+        $signerror = 1;
+        $emailerror = "email is required";
+    } else if (!filter_var($signemail, FILTER_VALIDATE_EMAIL)) {
+        $emailerror = "invalid email";
+        $signerror = 1;
+    } else {
+        $sql = "SELECT * FROM `account` WHERE `email` = '" . $signemail . "'";
+        $result = $connect->query($sql);
+        $present_row_count_in_db = mysqli_num_rows($result);
+        if ($present_row_count_in_db > 0) {
+            $emailerror = "email is already exist";
+            $signerror = 1;
+        }
+    }
+    if (empty($signpassword)) {
+        $passworderror = "password is required";
+        $signerror = 1;
+    }
+    if ($signerror == NULL) {
+        $sql = "INSERT INTO account VALUES ('$signname', '$signemail', '$signpassword')";
+        $result = $connect->query($sql);
+        if ($result) {
+            header('location:login.php');
+        }
     }
 }
 ?>
@@ -31,7 +62,18 @@ if (isset($_POST["sign"])) {
             font-family: sans-serif;
         }
 
-        a {}
+        .span {
+            border: 1px solid red;
+            margin: 0;
+        }
+
+        .section {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            width: 100%;
+        }
     </style>
 </head>
 
@@ -43,14 +85,30 @@ if (isset($_POST["sign"])) {
                 <h2>Welcome to Student daitails!</h2>
                 <p>Please sign-in to your account and start the adventure</p>
             </div>
-            <div class="login">
-                <input placeholder="Name" type="text" id="name" name="name" value="" />
+            <div class="section">
+                <div class="login">
+                    <input placeholder="Name" type="text" id="name" name="name" value="<?php echo $signname ?>" />
+                </div>
+                <div class="error" style="display:block">
+                    <?php echo $nameerror; ?>
+                </div>
             </div>
-            <div class="login">
-                <input placeholder="Email" type="email" id="email" name="email" value="" />
+            <div class="section">
+                <div class="login">
+                    <input placeholder="Email" type="email" id="email" name="email" value="<?php echo $signemail ?>" />
+                </div>
+                <div class="error" style="display:block">
+                    <?php echo $emailerror; ?>
+                </div>
             </div>
-            <div class="login">
-                <input placeholder="Password" type="password" id="password" name="password" />
+            <div class="section">
+                <div class="login">
+                    <input placeholder="Password" type="password" id="password" name="password"
+                        value="<?php echo $signpassword ?>" />
+                </div>
+                <div class="error" style="display:block">
+                    <?php echo $passworderror; ?>
+                </div>
             </div>
             <button class="login-btn" type="=sign" name="sign">SIGN UP</button>
             <div class="create-heading">If you have a Account? <span><a href="login.php">Login an account</a></span>
