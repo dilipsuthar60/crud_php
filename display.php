e23ffff63f7445498793c68a68dcd1b1e68d00a7
dilipsuthar60 <dilipsuthar212000@gmail.com>
2023-06-02 18:52:38 +0530

    remove space

--- display.php
+++ display.php
@@ -7,7 +7,6 @@ if (!isset($_SESSION["email"]) or !isset($_SESSION["password"]) or !isset($_SESS
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
